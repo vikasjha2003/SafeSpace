9730abc482f98876b907f017879629bbbb5d1e8e
Shivanshu Pandey <165388592+Shivanshu48@users.noreply.github.com>
2025-11-08 12:13:04 +0530

    create logout_php

---
+++ backend/logout.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+session_unset();
+session_destroy();
+
+// clear cookie (optional but helps)
+if (ini_get("session.use_cookies")) {
+  $params = session_get_cookie_params();
+  setcookie(session_name(), '', time() - 42000,
+    $params["path"], $params["domain"],
+    $params["secure"], $params["httponly"]
+  );
+}
+
+header("Location: ../index.php");
+exit();
+?>
